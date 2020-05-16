c57963ca83ef9c0573ebad5e5f7eb8f76e036b01
Gurpreetkaur77 <gurpreetk7sandhu@gmail.com>
2020-05-16 01:19:52 -0400

    First Commit

---
+++ simpleproject/src/main/java/simpleproject/Another.java
@@ -0,0 +1,10 @@
+package simpleproject;
+
+public class Another {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ simpleproject/src/main/java/simpleproject/Example.java
@@ -0,0 +1,9 @@
+package simpleproject;
+
+public class Example {
+
+	public static void main(String[] args) {
+	System.out.println("This is a demo of maven");
+	}
+
+}
